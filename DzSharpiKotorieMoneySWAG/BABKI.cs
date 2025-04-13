24d4d98049907a5a84db7790d32cd6a602085bdf
Artursdur <Manykian8889@gmail.com>
2025-04-13 18:49:16 +0500

    Добавьте файлы проекта.

---
+++ DzSharpiKotorieMoneySWAG/BABKI.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzSharpiKotorieMoneySWAG
+{
+    public class BABKI
+    {
+        public double Sum { get; set; }
+
+
+
+        public static implicit operator BABKI(double sum)
+        {
+            return new BABKI(sum);
+        }
+
+
+        public static BABKI operator +(BABKI num1, BABKI num2)
+        {
+            return new BABKI(num1.Sum + num2.Sum);
+        }
+        public static BABKI operator +(BABKI num1, double num2)
+        {
+            BABKI res = num1 + num2;
+            return res;
+        }
+
+        public static BABKI operator -(BABKI num1, BABKI num2)
+        {
+            return new BABKI(num1.Sum - num2.Sum);
+        }
+
+        public static BABKI operator -(BABKI num1, double num2)
+        {
+            BABKI res = num1 - num2;
+            return res;
+        }
+
+
+
+        public static BABKI operator *(BABKI num1, int num2)
+        {
+            return new BABKI(num1.Sum * num2);
+        }
+
+        public static BABKI operator /(BABKI num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("вы не можете разделить ни одно число на 0");
+            }
+            return new BABKI(num1.Sum / num2);
+        }
+
+
+
+        public static BABKI operator ++(BABKI num)
+        {
+            return new BABKI(num.Sum + 0.1);
+        }
+
+        public static BABKI operator --(BABKI num)
+        {
+
+            return new BABKI(num.Sum - 0.1);
+        }
+
+
+        public static bool operator >(BABKI num1, BABKI num2)
+        {
+            return num1.Sum > num2.Sum;
+        }
+
+        public static bool operator <(BABKI num1, BABKI num2)
+        {
+            return num1.Sum < num2.Sum;
+        }
+
+        public static bool operator ==(BABKI num1, BABKI num2)
+        {
+            return num1.Sum == num2.Sum;
+        }
+
+        public static bool operator !=(BABKI num1, BABKI num2)
+        {
+            return num1.Sum != num2.Sum;
+        }
+
+        public BABKI(double sum)
+        {
+            Sum = sum;
+        }
+
+        public bool IsNegative()
+        {
+            return Sum < 0;
+        }
+
+
+    }
+}
---
+++ DzSharpiKotorieMoneySWAG/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DzSharpiKotorieMoneySWAG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                BABKI num1 = 100;
+                BABKI num2 = 50;
+
+
+                try
+                {
+                    CheckResult(num2 - num1);
+                    CheckResult(num1 / 2);
+                    CheckResult(num1--);
+                    CheckResult(num2--);
+                }
+                catch (ApplicationException ex)
+                {
+                    throw new ApplicationException("oh no, you're bankrupt");
+                }
+            }
+
+            static void CheckResult(BABKI result)
+            {
+                if (result.Sum < 0)
+                {
+                    throw new ApplicationException("bankrupt");
+                }
+            }
+        }
+
+    }
+}
